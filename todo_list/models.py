a81ec15e2c055885c2507ba6f1d096db08e80f9a
Diana Shyrokikh <diashiroks@gmail.com>
2023-07-26 12:29:30 +0300

    Changed DateField on DateTimeField

--- todo_list/models.py
+++ todo_list/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Task(models.Model):
 
     name = models.CharField(max_length=255)
-    created_at = models.DateField(auto_now_add=True)
-    deadline = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     tags = models.ManyToManyField(
         to="Tag",
