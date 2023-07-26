456bc941124682d9e5707414f48087dbc8a401cf
Diana Shyrokikh <diashiroks@gmail.com>
2023-07-26 13:06:02 +0300

    Added change task status

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 
 from todo_list.views import (
+    ChangeStatus,
     IndexView,
 
 )
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tasks/<int:pk>/status/",
+         ChangeStatus.as_view(),
+         name="change-status"
+        ),
 
 ]
 
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from todo_list.models import Task
@@ -15,5 +15,23 @@ class IndexView(View):
             "tasks": tasks,
         }
         return render(request, "todo_list/index.html", context=context)
+
+
+class ChangeStatus(View):
+
+    @staticmethod
+    def get(request, pk):
+        tasks = Task.objects.prefetch_related("tags")
+        task = get_object_or_404(Task, pk=pk)
+        task.is_done = not task.is_done
+
+        task.save()
+
+        context = {
+            "tasks": tasks,
+        }
+
+        return render(request, "todo_list/index.html", context)
+
 
 
