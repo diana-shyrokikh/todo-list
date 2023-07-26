ca4f1405afe3febf89e839e14f24daa98e0fb038
Diana Shyrokikh <diashiroks@gmail.com>
2023-07-26 11:57:56 +0300

    Added todo_list urls

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include("todo_list.urls", namespace="todo_list")),
+    path("", include("todo_list.urls", namespace="todo_list")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
